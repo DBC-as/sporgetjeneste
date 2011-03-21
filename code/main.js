32cc15f360f4e48ae3fea6c26250f5ba3f200efc
rlo <rlo@b87a6194-a221-0410-ad6f-3b8ec35bba13>
2011-03-21 09:27:35 +0000

    text fixes
    git-svn-id: https://svn.dbc.dk/repos/skuffe/rje/sporgetjeneste@29110 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- code/main.js
+++ code/main.js
@@ -1,6 +1,6 @@
 function muiCallback(mui) {
     ({"start": function() {
-        mui.showPage(["page", {title: "Sp\xf8rgetjenesten"},
+        mui.showPage(["page", {title: "Sp\xf8rg biblioteket"},
             ["inputarea", {name: "question", label: "Mit sp\xf8rgsm\xe5l"}],
             ["choice", {name: "deadline", label: "Tidsfrist"},
                 ["option", {value: "-1"}, "ingen"],
@@ -14,15 +14,15 @@ function muiCallback(mui) {
                 ["option", {value: "business"}, "Erhverv"],
                 ["option", {value: "school1"}, "Folkeskole"],
                 ["option", {value: "school2"}, "Gymnasium, eller lignende"],
-                ["option", {value: "school3"}, "Laengere videregaaende uddannelse"],
+                ["option", {value: "school3"}, "L\u00e6ngere videreg\xe5ende uddannelse"],
                 ["option", {value: "school4"}, "Universitetsuddannelse eller forskning"]
             ],
             ["button", {id: "ask"}, "Sp\xf8rg"]
         ]);
     }, "ask": function() {
         mui.showPage(["page", {title: "Sp\xf8rgetjenesten"},
-            ["text", "Sp\xf8rgsm\xe5l afsendt. Du vil f\xe5 svar indenfor de naeste ", mui.form.deadline, " timer."],
-            ["button", {id: "start"}, "Nyt sp\xf8rgsmaal"]
+            ["text", "Sp\xf8rgsm\xe5l afsendt. Du vil f\xe5 svar indenfor de n\u00e6ste ", mui.form.deadline, " timer."],
+            ["button", {id: "start"}, "Nyt sp\xf8rgsm\xe5l"]
         ]);
     }})[mui.event]();
 
