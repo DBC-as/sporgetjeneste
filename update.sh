ecf7ec8198c021614428d2cabb0b55ae3e17d8b2
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-09-07 10:24:01 +0200

    bug#13311

---
+++ update.sh
@@ -0,0 +1,1 @@
+cp ../mui/js/COPYING ../mui/js/es5shim.js ../mui/js/jquery16min.js ../mui/js/jquerymobile.css ../mui/js/jquerymobile.js ../mui/js/json2.js ../mui/js/jsonml.js ../mui/js/mui.css ../mui/js/mui.js mui
